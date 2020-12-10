6bba514f3b661b27811c0464b910fd443865e1ae
Isaac Aderonmu <aderonmuisaac@gmail.com>
2020-12-10 19:44:51 +0100

    feat: created landing page

---
+++ src/pages/landingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HeaderContainer } from '../containers';
+
+export default function LandingPage() {
+    return(
+        <>
+            <HeaderContainer />
+        </>
+    );
+}
