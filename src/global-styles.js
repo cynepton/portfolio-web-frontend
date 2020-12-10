9e01391610461932e1b3d74ac1d5e2ad2e376aa0
Isaac Aderonmu <aderonmuisaac@gmail.com>
2020-12-10 19:44:28 +0100

    feat: define global styles

---
+++ src/global-styles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+import { 
+    brandColorBackground, brandColorText 
+} from './constants/styleConstants';
+
+export const GlobalStyles = createGlobalStyle`
+    html, body {
+        font-family: 'Poppins', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        background-color: ${brandColorBackground};
+        color: ${brandColorText};
+        font-size: 16px;
+    }
+`;
