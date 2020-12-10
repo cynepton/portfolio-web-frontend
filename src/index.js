f956f1ade0f539ddc11123cbe30aaf9295b89fa3
Isaac Aderonmu <aderonmuisaac@gmail.com>
2020-12-10 19:46:19 +0100

    feat: updated index.js
    - imported render directly from react-dom
    - imported normalize.css
    - imported globalstyles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import 'normalize.css';
+import { GlobalStyles } from './global-styles';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+render(
+    <React.StrictMode>
+        <GlobalStyles />
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
