106cfadfdb1e30741997b8f38134a08bf5496357
Isaac Aderonmu <aderonmuisaac@gmail.com>
2020-12-11 09:48:20 +0100

    feat: added nav items

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Header } from '../../components';
 import * as ROUTES from '../../constants/routes';
-import logo from '../../logo-white.svg'
+import logo from '../../logo-white.svg';
+import navItems from '../../fixtures/navSections.json';
 
 export default function HeaderContainer() {
     return(
         <Header>
             <Header.Logo to={ROUTES.HOME} alt="Isaac Aderonmu" src={logo} />
             <Header.Spacer />
-            <Header.Navigation>Navigate</Header.Navigation>
+            <Header.Navigation>
+                {navItems.map((item) =>(
+                    <Header.NavItem href={'#'.concat(item.id)}>{item.name}</Header.NavItem>
+                ))}
+            </Header.Navigation>
         </Header>
     )
 }
