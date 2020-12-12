f41b772ce8c065c2c84c478ee543d833c5aed773
Isaac Aderonmu <aderonmuisaac@gmail.com>
2020-12-12 19:27:23 +0100

    feat: changed background color

--- src/constants/styleConstants.js
+++ src/constants/styleConstants.js
@@ -1,6 +1,6 @@
 // Style constants for the application
 
-export const brandColorBackground = "#565252";
+export const brandColorBackground = "#231f20";
 export const brandColorPrimary = "#ea8bdb";
 export const brandColorSecondary = "#f96f06";
 export const brandColorText = "#ffffff";
