2c5493b511e01fd38cff6543c1df8d790691b11a
Isaac Aderonmu <aderonmuisaac@gmail.com>
2021-02-18 12:15:46 +0100

    feat: add color and text styles

---
+++ src/constants/colorStyles.js
@@ -0,0 +1,29 @@
+/**
+ * Brand Purple color
+ */
+export const brandColor1 = "#F8A5FF";
+// export const brandColor1 = "#BB6BD9";
+/**
+ * Brand White Color
+ */
+export const brandColor2 = "#FFFFFF";
+/**
+ * Brand Almost Black Color
+ */
+export const brandColor3 = "#272727";
+/**
+ * Brand Black Color
+ */
+export const brandColor4 = "#000000";
+/**
+ * Text White Color
+ */
+export const textColor1 = "#FFFFFF";
+/**
+ * Text Black Color
+ */
+export const textColor2 = "#000000";
+/**
+ * Transparent Black Color
+ */
+export const darkGlass1 = "rgba(39, 39, 39, 0.72)";
---
+++ src/constants/textStyles.js
@@ -0,0 +1,25 @@
+// Text styles
+// To use, Use the string as the value for the font css property.
+
+/**
+ * Menu Items Text
+ *
+ * `font-style` `font-weight` `font-size`/`line-height` `font-family`
+ */
+export const menuItemText = "normal 400 16px/24px Poppins";
+export const menuItemTextHover = "normal 500 17px/24px Poppins";
+
+
+// JUMBOTRON TEXT
+/**
+ * Light Jumbotron Text
+ *
+ * `font-style` `font-weight` `font-size`/`line-height` `font-family`
+ */
+export const regularJumboText = "normal 300 24px/24px Poppins";
+/**
+ * Largest Jumbotron Text
+ *
+ * `font-style` `font-weight` `font-size`/`line-height` `font-family`
+ */
+export const kingJumboText = "normal bold 116px/116px Poppins";
