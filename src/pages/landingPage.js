5e36b97f729e36feb231ba5cb353cb3f08014ab4
Isaac Aderonmu <aderonmuisaac@gmail.com>
2021-02-18 12:18:28 +0100

    feat: construct landing page

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import {
+    HeaderContainer,
+    JumbotronContainer
+} from '../containers';
+import { HOME } from '../constants/routes';
 
-const LandingPage = () => {
+const LandingPage = (props) => {
+
+    const textList = [
+        { text: "Hello, I'm", size: "regular" },
+        { text: "ISAAC ADERONMU,", size: "king" },
+        { text: "I am full stack developer, product designer, and robotics software engineer.", size: "regular" }
+    ];
+
     return(
         <>
+            <HeaderContainer currentPageID={HOME.pageID} />
+            <JumbotronContainer
+                textList={textList}
+            />
         </>
     );
 }
